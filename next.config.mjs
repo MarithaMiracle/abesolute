ebb7926b7e2ca208b3d6cd451c50fbedd1ef7e04
Maritha Miracle <marithamiracle@gmail.com>
2026-03-07 04:15:03 +0100

    Website build

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,7 @@
 const nextConfig = {
     images: {
         formats: ['image/webp', 'image/avif'],
-    },
-    experimental: {
-        optimizeCss: process.env.NODE_ENV === 'production',
-    },
+    }
 };
 
 export default nextConfig;
